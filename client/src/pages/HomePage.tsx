cc01c2b97a53bc7d6721645b714b7b541596dac9
Abhishek <Abhishekraut8697@gmail.com>
2025-03-04 20:46:14 +0530

    Add HomePage layout with Sidebar, ChatContainer, and NoChatSelected components

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,21 @@
+import { Sidebar } from "lucide-react"
+import UseChatStore from "../store/UseChatStore"
+import NoChatSelected from "../components/NoChatSelected"
+
 const HomePage = () => {
-
-
+  const {selectedUser} = UseChatStore()
   return (
-    <div className="pt-20 h-screen">Homepage</div>
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4 bg-gray-400">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="h-full flex rounded-lg overflow-hidden bg-green-900">
+            <Sidebar/>
+
+            {! selectedUser : <NoChatSelected/> : <ChatContainer/>}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
