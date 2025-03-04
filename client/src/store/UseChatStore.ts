5f576f35fe928b5afd2eae41808d262403935df1
Abhishek <Abhishekraut8697@gmail.com>
2025-03-04 20:31:07 +0530

    Refactor UseChatStore to enhance error handling and integrate toast notifications for user feedback

--- client/src/store/UseChatStore.ts
+++ client/src/store/UseChatStore.ts
@@ -1,53 +1,57 @@
 import { create } from "zustand";
 import { AxiosInstance } from "../lib/AxiosInstance";
 import { handleError } from "./useAuthStore";
+import toast from "react-hot-toast";
 
-interface IUseChatStore{
-    messages :string[];
-    users:string[];
-    selectedUser:null;
-    isUsersLoading:boolean;
-    isMessagesLoading:boolean;
-    error: string | null,
+interface IUseChatStore {
+  messages: string[];
+  users: string[];
+  selectedUser: null;
+  isUsersLoading: boolean;
+  isMessagesLoading: boolean;
+  error: string | null;
 
-    getUsers: ()=>Promise<void>
-    getMessages: (id:string)=>Promise<void>
+  getUsers: () => Promise<void>;
+  getMessages: (id: string) => Promise<void>;
 }
 
-const UseChatStore = create<IUseChatStore>((set)=>({
-    messages:[],
-    users:[],
-    selectedUser:null,
-    isUsersLoading:false,
-    isMessagesLoading:false,
-    error: null,
+const UseChatStore = create<IUseChatStore>((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+  error: null,
 
+  getUsers: async () => {
+    set({ isUsersLoading: true, error: null });
+    try {
+      const res = await AxiosInstance.get("/message/getUsersInChat");
+      set({ users: res.data.data });
+    } catch (error) {
+      console.log("error occured while getting usersInChat", error);
+      const errorMessage = handleError(error);
+      toast.error(errorMessage);
+      set({ error: errorMessage });
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
 
-    getUsers: async ()=>{
-        set({ isUsersLoading:true , error:null })
-        try {
-            const res =await AxiosInstance.get('/message/getUsersInChat')
-            set({users:res.data.data})
-        } catch (error) {
-            console.log("error occured while getting usersInChat",error)
-            set({isUsersLoading:false , error: handleError(error)} )
-        }finally{
-            set({isUsersLoading:false})
-        }
-    },
-
-    getMessages: async (id)=>{
-        set({ isMessagesLoading:true , error:null })
-        try {
-            const res =await AxiosInstance.get(`/message/${id}`)
-            set({messages:res.data.data})
-        } catch (error) {
-            console.log("error occured while getting messages",error)
-            set({isMessagesLoading:false , error: handleError(error)} )
-        }finally{
-            set({isMessagesLoading:false})
-        }
-    },
+  getMessages: async (id) => {
+    set({ isMessagesLoading: true, error: null });
+    try {
+      const res = await AxiosInstance.get(`/message/${id}`);
+      set({ messages: res.data.data });
+    } catch (error) {
+      console.log("error occured while getting messages", error);
+      const errorMessage = handleError(error);
+      toast.error(errorMessage);
+      set({ error: errorMessage });
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
 }));
 
-export default UseChatStore
+export default UseChatStore;
