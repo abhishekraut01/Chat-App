ade1b117479dc143a27fd2fbfe926acefad0fa4c
Abhishek <Abhishekraut8697@gmail.com>
2025-03-04 20:51:09 +0530

    Enhance NoChatSelected component with welcome message and icon; update HomePage layout to integrate Sidebar and ChatContainer

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,19 @@
-import { Sidebar } from "lucide-react"
+
 import UseChatStore from "../store/UseChatStore"
 import NoChatSelected from "../components/NoChatSelected"
+import ChatContainer from "../components/ChatContainer"
+import Sidebar from "../components/Sidebar"
 
 const HomePage = () => {
   const {selectedUser} = UseChatStore()
   return (
     <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4 bg-gray-400">
-        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="h-full flex rounded-lg overflow-hidden bg-green-900">
+      <div className="flex items-center justify-center pt-20 px-4 ">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-8rem)] ">
+          <div className="h-full flex rounded-lg overflow-hidden">
             <Sidebar/>
 
-            {! selectedUser : <NoChatSelected/> : <ChatContainer/>}
+            {! selectedUser ? <NoChatSelected/> : <ChatContainer/>}
           </div>
         </div>
       </div>
