02f45a11587cea2a96941a239cd63e625938ba50
Abhishek <Abhishekraut8697@gmail.com>
2025-03-08 20:36:55 +0530

    Enhance ChatInput component: add image preview functionality and integrate sendMessages method from UseChatStore

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,11 +1,49 @@
 import { useRef, useState } from "react";
+import UseChatStore from "../store/UseChatStore";
+import { X } from "lucide-react";
 
 const ChatInput = () => {
   const [text, setText] = useState("");
-  const [img, setImg] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
   const fileInputRef = useRef(null);
+  const {sendMessages} =   UseChatStore()
 
-  return <div>ChatInput</div>;
+  const handleImageChange = ()=>{
+
+  }
+
+  const removeImage = ()=>{
+    
+  }
+
+  const handleSendMessage =async ()=>{
+    
+  }
+
+  return (
+    <div className=" p-4 w-full">
+         {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+            
+    </div>
+  );
 };
 
 export default ChatInput;
