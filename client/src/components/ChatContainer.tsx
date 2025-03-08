f659be045931a2f901bf1fae7638a741773cb97e
Abhishek <Abhishekraut8697@gmail.com>
2025-03-08 20:13:22 +0530

    Refactor ChatContainer to include ChatHeader and ChatInput during loading state; enhance loading logic and state management in ChatInput

--- client/src/components/ChatContainer.tsx
+++ client/src/components/ChatContainer.tsx
@@ -1,26 +1,35 @@
-import { useEffect } from "react"
-import UseChatStore from "../store/UseChatStore"
-import MessageSkeleton from "./skeletons/MessageSkeleton"
-import ChatHeader from "./ChatHeader"
-import ChatInput from "./ChatInput"
+import { useEffect } from "react";
+import UseChatStore from "../store/UseChatStore";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import ChatHeader from "./ChatHeader";
+import ChatInput from "./ChatInput";
 
 const ChatContainer = () => {
-  const {messages , getMessages , selectedUser , setSelectedUser , isMessagesLoading}= UseChatStore()
+  const { messages, getMessages, selectedUser, isMessagesLoading } =
+    UseChatStore();
 
-  useEffect(()=>{
+  useEffect(() => {
     if (selectedUser?._id) {
       getMessages(selectedUser._id);
     }
-  },[selectedUser , getMessages])
+  }, [selectedUser, getMessages]);
 
-  if (isMessagesLoading) return <MessageSkeleton/>
+  if (isMessagesLoading) {
+    return (
+      <div className="flex flex-1 flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <ChatInput />
+      </div>
+    );
+  }
   return (
     <div className="flex flex-1 flex-col overflow-auto">
-      <ChatHeader/>
+      <ChatHeader />
       <p>messages.....</p>
-      <ChatInput/>
+      <ChatInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,6 +1,9 @@
-
+import { useRef, useState } from "react"
 
 const ChatInput = () => {
+    const [text , setText] = useState("")
+    const [img , setImg] = useState(null)
+    const fileInputRef = useRef()
   return (
     <div>ChatInput</div>
   )
